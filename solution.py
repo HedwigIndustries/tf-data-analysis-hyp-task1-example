113a9cd05bfd7bb612610e2cc094bb95f220da2c
Rustam Kadyrov <rustam_kadyrov_03@mail.ru>
2024-05-25 20:50:18 +0300

    task1 solution

--- solution.py
+++ solution.py
@@ -1,14 +1,12 @@
-import pandas as pd
-import numpy as np
+from scipy import stats
+
+chat_id = 886180056
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
+def solution(x_success: int,
+             x_cnt: int,
+             y_success: int,
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    z_stat, p_value = stats.proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='two-sided')
+    alpha = 0.08
+    return p_value < alpha
